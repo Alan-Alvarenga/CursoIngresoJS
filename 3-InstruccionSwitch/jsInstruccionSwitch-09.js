f6e2f1d63998a6e2753409377a7a05b59c23b2b3
Alan-Alvarenga <a.alvarengautn@gmail.com>
2021-02-15 16:38:07 -0300

    prueba

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -3,32 +3,58 @@ function mostrar()
 	let estacion= document.getElementById('txtIdEstacion').value;
 	let destino= document.getElementById("txtIdDestino").value;
 	let precio= 15000;
-	let descuento;
-	let aumento;
-	let descuentoTotal;
-	let aumentoTotal;
+	let precioFinal
 
 	switch (estacion) {
 		case "Invierno":
 			switch (destino) {
 				case "Bariloche":
-				aumento=1.2;
+				precioFinal= precio*1.2;
 				break;
 
 				case "Cordoba":
 				case "Cataratas":
-				descuento=0.9;
+				precioFinal=precio*0.9;
 				break;
 
 				case "Mar del plata":
-				descuento=0.8;
+				precioFinal=precio*0.8;
 				break;
 			}
-		
+		break;
+
 		case "Verano":
 			switch (destino) {
-				case "Bariloche"
+				case "Bariloche":
+				precioFinal=precio*0.8;
+				break;
+
+				case "Cordoba":
+				case "Cataratas":
+				precioFinal=precio*1.1;
+				break;
+
+				case "Mar del plata":
+				precioFinal=precio*1.2;
+				break;
+			}
+		break;
+
+		case "Otoño":
+		case "Primavera":
+			switch (destino) {
+				case "Bariloche":
+				case "Cataratas":
+				case "Mar del plata":
+				precioFinal=precio*1.1;
+				break;
+
+				default:
+				precioFinal=precio;
+				break;
 			}
 	}
+
+	alert("El precio final es $" + precioFinal)
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,49 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let estacion=document.getElementById('txtIdEstacion').value;
+	let destino= document.getElementById("txtIdDestino").value;
+	let mensaje;
+
+	switch (estacion) {
+		case "Invierno":
+			switch (destino) {
+				case "Bariloche":
+				mensaje= "Se viaja";
+				break;
+
+				default:
+				mensaje= "No se viaja";
+			}
+		break;
+
+		case "Verano":
+			switch (destino) {
+				case "Cataratas":
+				case "Mar del plata":
+				mensaje= "Se viaja";
+				break;
+
+				default:
+				mensaje= "No se viaja";
+			}
+		break;
+
+		case "Otoño":
+		mensaje= "Se viaja";
+		break;
+
+		case "Primavera":
+			switch (destino) {
+				case "Bariloche":
+				mensaje= "No se viaja";
+				break;
+
+				default:
+				mensaje= "Se viaja";
+			}
+		break;
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
