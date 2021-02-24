d6e22c59c7b4c7a97d26135779d417dfda5378d6
Alan-Alvarenga <a.alvarengautn@gmail.com>
2021-02-23 22:29:50 -0300

    Clase 9

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -13,17 +13,50 @@ hasta que el usuario quiera, mostrar:
 function mostrar()
 {
 	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
+	let respuesta="si";
+	let numeroIngresado;
+	let sumaNegativos=0;
+	let sumaPositivos=0;
+	let cantNegativos=0;
+	let cantPositivos=0;
+	let cantCeros=0;
+	let cantPares=0;
+	let promNegativos;
+	let promPositivos;
+	let difPosNeg;
 
-	respuesta="si";
+	while (respuesta=="si") {
+		numeroIngresado= parseInt (prompt ("Ingrese un numero"))
 
-	while(respuesta=="si")
-	{
-		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+		if (numeroIngresado<0) {
+			sumaNegativos= sumaNegativos + numeroIngresado
+			cantNegativos= cantNegativos++
+			promNegativos= sumaNegativos/cantNegativos
+		}
+
+		if (numeroIngresado>0) {
+			sumaPositivos= sumaPositivos + numeroIngresado
+			cantPositivos= cantNegativos++
+			promPositivos= sumaPositivos/cantPositivos
+		}
+
+		else {
+			cantCeros= cantCeros++
+		}
+
+		respuesta= prompt ("Desea seguir? si/no")
+	}
+
+	difPosNeg= sumaPositivos - sumaNegativos
 
 	document.write("la suma de negativos es :"+sumaNegativos);
+	document.write("la suma de positivos es :"+sumaPositivos);
+	document.write("la cantidad de negativos es :"+cantNegativos);
+	document.write("la cantidad de positivos es :"+cantPositivos);
+	document.write("la cantidad de ceros es :"+cantCeros);
+	document.write("la cantidad de numero par es :");
+	document.write("el promedio de negativos es :"+promNegativos);
+	document.write("el promedio de positivos es :"+promPositivos);
+	document.write("la diferencia de positivos y negativos es :"+difPosNeg);
+
 }//FIN DE LA FUNCIÓN
